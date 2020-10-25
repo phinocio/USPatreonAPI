f1ed7996c2b818e71b3f3c4d39f0142b79b5451a
Phinocio <github@phinocio.com>
2020-10-25 16:07:46 -0600

    progress on getting posts

--- app/Http/Controllers/PatreonController.php
+++ app/Http/Controllers/PatreonController.php
@@ -3,21 +3,68 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class PatreonController extends Controller
 {
 
     public static function getPatrons() {
-		$access_token = env('PATREON_TOKEN', false);
-		
+		$access_token = env('PATREON_TOKEN');
 		$api_client = new \Patreon\API($access_token);
 		$campaign_response = $api_client->fetch_campaigns();
-		dd($campaign_response);
-		// $campaign = $campaign_response->get('data')->get('0');
-		// echo "campaign is\n";
-		// print_r($campaign->asArray(true));
-		// $user = $campaign->relationship('creator')->resolve($campaign_response);
-		// echo "user is\n";
-		// print_r($user->asArray(true));
+		$campaign_id = $campaign_response['data'][0]['id'];
+		//$details = $api_client->fetch_campaign_details($campaign_id);
+
+		$resp = Http::withToken($access_token)->get('https://patreon.com/api/oauth2/v2/campaigns/' . $campaign_id . '/posts?fields[post]=title,content,is_public,published_at,url&page[cursor]=oO1VaaHnEyhUIq13xaX688_BgsM');
+
+		// $resp = Http::withToken($access_token)->get('https://patreon.com/api/oauth2/v2/members/006defe4-1750-445b-843c-2f993db27272?fields[member]=full_name');
+
+		// $resp = Http::withToken($access_token)->get('
+		https://patreon.com/api/oauth2/v2/campaigns/352583/posts?fields[post]=title,Ccontent,is_public,published_at,url&page[cursor]=oO1VaaHnEyhUIq13xaX688_BgsM');
+
+		$posts = $resp->json();
+
+		// foreach ($posts['data'] as $post)
+		// {
+		// 	echo $post['attributes']['title'] . "<br>";
+		// }
+
+		// echo $posts['links']['next'];
+
+	}
+
+	public static function getPosts($url, $access_token) {
+		$allPosts = [];
+		$resp = Http::withToken($access_token)->get($url);
+		
+
+		foreach ($resp['data'] as $post) {
+			if ($post['attributes']['is_public'] != false) {
+				array_push($allPosts, $post);
+			}
+		}
+		$nextLink = $resp['links']['next'];
+
+		while($nextLink != false)
+		{
+			$resp = Http::withToken($access_token)->get($nextLink);
+			
+			foreach ($resp['data'] as $post) {
+				if ($post['attributes']['is_public'] != false) {
+					array_push($allPosts, $post);
+				}
+			}
+
+			if(isset($resp['links']['next'])) {
+				$nextLink = $resp['links']['next'];
+			} else {
+				$nextLink = false;
+			}
+			dump($resp->json());
+		}
+
+		dd($allPosts);
+
+		return $allPosts;
 	}
 }
--- app/Http/Controllers/ResponseController.php
+++ app/Http/Controllers/ResponseController.php
@@ -9,15 +9,19 @@ use Exception;
 class ResponseController extends Controller
 {
 	public function index() {
+		$access_token = env('PATREON_TOKEN');
+		$api_client = new \Patreon\API($access_token);
+		$campaign_response = $api_client->fetch_campaigns();
+		$campaign_id = $campaign_response['data'][0]['id'];
+		$postsUrl = 'https://patreon.com/api/oauth2/v2/campaigns/' . $campaign_id . '/posts?fields[post]=title,content,is_public,published_at,url';
 		try {
-			$response = PatreonController::getPatrons();
+			$posts = PatreonController::getPosts($postsUrl, $access_token);
+			//$patrons = PatreonController::getPatrons();
+
+			dd($posts);
 		} catch (Exception $e) {
 			var_dump($e);
 		}
-		return response()->json([
-			'name' => 'Abigail',
-			'state' => 'CA',
-		]);
 	}
     
 }
